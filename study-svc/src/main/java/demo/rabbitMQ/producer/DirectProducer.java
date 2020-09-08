28a1e50947314d0cdea4c6c328f86219e4a5fc4b
liliang <18871966597@163.com>
2020-09-08 18:01:00 +0800

    消息发送方确认模式

--- study-svc/src/main/java/demo/rabbitMQ/producer/DirectProducer.java
+++ study-svc/src/main/java/demo/rabbitMQ/producer/DirectProducer.java
@@ -1,9 +1,12 @@
 package demo.rabbitMQ.producer;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ReturnListener;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -29,6 +32,40 @@ public class DirectProducer {
         Connection connection = factory.newConnection();
 
         Channel channel = connection.createChannel();
+        // 生产者确认模式开启设置
+        channel.confirmSelect();
+        // 异步确认需要实现对信道监听并实现返回确认消息时候的做法和返回拒绝消息的做法
+        channel.addConfirmListener(new ConfirmListener() {
+            // deliverytag 消息主键（代表一次唯一的投递）,单调递增的正整数
+            // multiple 批处理,默认为false
+            @Override
+            public void handleAck(long deliverytag, boolean multiple) throws IOException {
+                System.out.println("ACK = deliverytag = " + deliverytag + "multiple = " + multiple);
+            }
+
+            @Override
+            public void handleNack(long deliverytag, boolean multiple) throws IOException {
+                System.out.println("ACK = deliverytag = " + deliverytag + "multiple = " + multiple);
+            }
+        });
+
+        // 无法找到合适队列时会调用这个returnListener
+        //1、mandatory参数为true，投递消息时无法找到一个合适的队列
+        //消息返回给生产者
+        //false 丢弃消息(缺省)
+        channel.addReturnListener(new ReturnListener() {
+            @Override
+            public void handleReturn(int replyCode, String replyText,
+                                     String exchange, String routingKey,
+                                     AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String msg = new String(body);
+                System.out.println("replyText:"+replyText);
+                System.out.println("exchange:"+exchange);
+                System.out.println("routingKey:"+routingKey);
+                System.out.println("msg:"+msg);
+            }
+        });
 
         // channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
         channel.exchangeDeclare(TOPIC_EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
@@ -38,8 +75,18 @@ public class DirectProducer {
             String server = info[1];
             String message = "hello World" + (i + 1);
 
-            channel.basicPublish(TOPIC_EXCHANGE_NAME, server, null, message.getBytes());
+            // 同步不需要设置mandatory参数缺省默认为false
+            // channel.basicPublish(TOPIC_EXCHANGE_NAME, server, null, message.getBytes());
+            channel.basicPublish(TOPIC_EXCHANGE_NAME, server, true,null, message.getBytes());
             System.out.println("Send " + server + ":" + message);
+            // 生产者发送确认模式 同步机制实现会一直阻塞
+           /* try {
+                if(channel.waitForConfirms()){
+                    System.out.println("消息发送成功！！！");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
 
         }
 
